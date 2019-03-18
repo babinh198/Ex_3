2655ec64c8899ea5ab50e029e1b5b24f0dac6e7e
babinh198 <48363973+babinh198@users.noreply.github.com>
2019-03-19 01:01:48 +0700

    Create bai6.py

---
+++ bai6.py
@@ -0,0 +1,14 @@
+number = 2 ** 10000
+result = []
+for num in str(number):
+    result.append(int(num))
+print(sum(result))
+
+#13561
+
+number = 2 ** 10000
+sum_ = 0
+for num in str(number):
+    sum_ += int(num)
+print(sum_)
+#####################################################
